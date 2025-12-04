6535078351571ac3aed75ba89bb5c65c305c4615
jtripp99 <168134354+jtripp99@users.noreply.github.com>
2025-12-04 14:31:35 -0700

    Delete src directory

--- src/switch.h
+++
@@ -1,9 +0,0 @@
-// Description: Function prototypes for switch.cpp
-//----------------------------------------------------------------------//
-
-#ifndef SWITCH_H
-#define SWITCH_H
-
-void initSwitchPB3();
-
-#endif
