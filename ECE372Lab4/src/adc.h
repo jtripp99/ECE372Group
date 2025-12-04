7019f07a3dc2e60ce787508044fc25ab22078371
arkrock198 <59707638+arkrock198@users.noreply.github.com>
2025-12-04 14:07:26 -0700

    Delete ECE372Lab4 directory

--- ECE372Lab4/src/adc.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef ADC_H
-#define ADC_H
-
-
-void initADC7();
-//function to read ADC7
-
-#endif
--- ECE372Lab4/src/sevensegmentdisplay.h
+++
@@ -1,9 +0,0 @@
-#ifndef SEVENSEGDISPLAY_H
-#define SEVENSEGDISPLAY_H
-
-#include <avr/io.h>
-
-void init7Seg();
-void setNum(unsigned char num);
-
-#endif
