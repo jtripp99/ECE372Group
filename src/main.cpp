4dd9d3c22392df6a758a8888bac89ed889758e75
maithammaithamtaleb-cyber <maithammaithamtaleb@gmail.com>
2025-12-05 13:40:47 -0700

    Refactor main.cpp to implement sound sensor logic

--- src/main.cpp
+++ src/main.cpp
@@ -9,29 +9,55 @@
 #include "soundsensor.h"
 #include "timer.h"
 
-typedef enum sound_states {
-    NORMAL, ISTOOLOUD
-} sound_stateType;
-tilt_stateType tiltState = NORMAL;
-
 typedef enum button_states {
   wait_press, debounce_press, wait_release, debounce_release
 } stateType;
 stateType state = wait_press;
 
-// put function declarations here:
-int myFunction(int, int);
+typedef enum sound_sensor_states {
+    wait_sound, debounce_sound, wait_no_sound, debounce_no_sound
+} soundstateType;
+soundstateType soundstate = wait_sound;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+int main(){
+    sei(); // Enable global interrupts
+    init_PB4();
+    Serial.begin(9600);
+    while(1) {
+
+        // //printing value of sound sensor state machine
+        // Serial.print("Sound Sensor State: ");
+        // Serial.println((PINB &(1 << PINB4)) ? 1 : 0);
+
+        if (PINB &(1 << PINB4)) {
+            Serial.print("Sound Detected\n");
+        }
+        switch(soundstate) {
+            case wait_sound:
+                break;
+            case debounce_sound:
+                //ms_delay(1);//Timer 1
+                soundstate = wait_no_sound;
+                break;
+            case wait_no_sound:
+                break;
+            case debounce_no_sound:
+                //ms_delay(1);
+                soundstate = wait_sound;
+
+            break;
+        }
+
+    }
+    return 0;
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+ISR(PCINT0_vect){
+    if(soundstate == wait_sound){
+        Serial.print("Sound Detected in ISR\n");
+        soundstate = debounce_sound; // Transition to debounce_press state
+    }
+    else if(soundstate == wait_no_sound){
+        Serial.print("No Longer Detected\n");
+        soundstate = debounce_no_sound; // Transition to debounce_release state
+    }
 }
