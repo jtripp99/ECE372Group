0c8078b8de0993a115659d9ee6970db7f4fe9a2e
maithammaithamtaleb-cyber <maithammaithamtaleb@gmail.com>
2025-12-06 14:48:58 -0700

    Rename init_PB4 to init_PB1 and update function

--- src/soundsensor.cpp
+++ src/soundsensor.cpp
@@ -1,11 +1,12 @@
 #include "soundsensor.h"
 #include <avr/io.h>
 
-void init_PB4(){
+void init_PB1(){
     DDRB &= ~(1<<DDB1); // I/P
     PORTB &= ~(1<<PORTB1); //possibly disable pull-up
     PCICR |= (1 << PCIE0); // enable PCINT 0 - 7
     PCMSK1 |= (1 << PCINT1); // enable PCINT4
 
 }
+
 
